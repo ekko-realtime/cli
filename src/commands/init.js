2ad9974e360da1ad3fd76969ef43ddb64a4ce01a
Drew Holbrook <drewholbrook@gmail.com>
2021-04-27 16:08:57 -0600

    invoked AWS credentials method in init command

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,11 +1,9 @@
-const { Command, flags } = require("@oclif/command");
+const { Command } = require("@oclif/command");
 const { cli } = require("cli-ux");
-const process = require("process");
 const {
   updateAWSCredentials,
   createBlankEkkoDirectory,
 } = require("../../util/fileUtil.js");
-const deployFunction = require("../../util/deployFunction.js");
 const ekkoInit = require("../../util/ekkoInit.js");
 
 class InitCommand extends Command {
@@ -16,30 +14,18 @@ class InitCommand extends Command {
     response = response.toLowerCase();
 
     if (response === "y") {
-      // updateAWSCredentials(CONTENT);
+      updateAWSCredentials(CONTENT);
       ekkoInit();
     } else if (response === "n") {
-      // updateAWSCredentials(CONTENT);
+      updateAWSCredentials(CONTENT);
       createBlankEkkoDirectory();
     }
-    // const AWS_ACCESS_KEY_ID = await cli.prompt(
-    //   "Please enter your AWS ACCESS KEY ID"
-    // );
-    // const AWS_SECRET_KEY = await cli.prompt("Please enter your AWS SECRET Key");
-    // const AWS_REGION = await cli.prompt("Please enter your AWS REGION");
-    // const CONTENT = `AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\nAWS_SECRET_KEY=${AWS_SECRET_KEY}\nAWS_REGION=${AWS_REGION}`;
-
-    // FileUtil.saveAWSCredentials(CONTENT);
-
-    // FileUtil.createEkkoFunctionsDirectory();
-    // process.chdir("./ekko_functions");
-    // deployFunction("sampleEkkoFunction");
   }
 }
 
-InitCommand.description = `Create ekko_functions directory, associations.json, and sampleEkkoFunction that gets deployed to AWS Lambda with your AWS credentials 
+InitCommand.description = `Initialize a new ekko project or join an existing one 
 ...
-Creates ekko_functions directory, associations.json, and sampleEkkoFunction that gets deployed to AWS Lambda with your AWS credentials
+Initializes a new ekko project or join an existing one
 `;
 
 module.exports = InitCommand;
