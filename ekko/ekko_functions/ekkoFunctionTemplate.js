81d443bee5aca50128dbc5ecc330c9b10d81ff22
Drew Holbrook <drewholbrook@gmail.com>
2021-05-03 07:23:54 -0600

    deleted sample ekko directory from init (didn't belong in repo)

--- ekko/ekko_functions/ekkoFunctionTemplate.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello from ekko generated Lambda!"),
-  };
-  return response;
-}
--- src/util/doSomething.js
+++ src/util/doSomething.js
@@ -2,14 +2,13 @@ const childProcess = require("child_process");
 const repo = "https://github.com/ekko-live/ekko-init.git";
 
 const doSomething = async () => {
-  // console.log("HI");
-  // childProcess.execSync(`git clone -q '${repo}' SMEKKO`);
-  // console.log("hello");
-  setTimeout(() => {
-    // console.log("3 seconds");
-  }, 3000);
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("foo");
+    }, 3000);
+  });
 
-  return "message";
+  return myPromise;
 };
 
 module.exports = doSomething;
