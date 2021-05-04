c18c71d09d2ca103d878796365d9dbc544994579
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-05-04 17:57:32 -0400

    update create

--- src/commands/create.js
+++ src/commands/create.js
@@ -19,6 +19,7 @@ class CreateCommand extends Command {
 CreateCommand.description = `Create a local ekko function
 ...
 Pass in the name of the function you want to create as an argument to this command. It will create:
+
 - a folder in the ekko_functions directory and
 - a skeleton JavaScript template file (inside the folder it creates) that you can use to get started writing your Lambda function code.
 `;
